92d5b26813f8c69ebcb690035e4892683a204c4a
supersung <bjs73370@gmail.com>
2023-09-04 17:58:35 +0900

    add file mod

---
+++ edittest.py
@@ -0,0 +1,13 @@
+a = "hello"
+b = "world"
+
+print(a, b)
+
+print(a)
+print(a,b)
+number_school = 2394584903
+print("number_school")
+bool_view = False
+print(bool_view)
+char_element = 'a'
+print(char_element)
