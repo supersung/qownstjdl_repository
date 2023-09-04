2f43a411fa83f3c2db28e7813dcf74095de8fb20
unknown <bjs73370@naver.com>
2023-09-05 01:37:00 +0900

    python

---
+++ edittest.py
@@ -0,0 +1,12 @@
+bool_view = False
+print(bool_view)
+flt_pi = 3.14
+print(flt_pi)
+char_element ='a'
+print(char_element)
+chars = "hello world"
+print(chars)
+number_school = 2394584903
+print(number_school)
+str_name = "python"
+print(str_name)
