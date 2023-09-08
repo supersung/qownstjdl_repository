7b1369832645d1b0ef4929eaab3848e84f7932fd
unknown <bjs73370@gmail.com>
2023-09-08 17:52:48 +0900

    딕셔너리까지 2023/09/08

--- var.py
+++ var.py
@@ -73,3 +73,130 @@ my_list.insert(2,4)
 print(my_list)
 
 print(my_list.count(8))
+
+my_list.append(8)
+print(my_list.count(8))
+
+print(my_list.pop())
+print(my_list)
+print(my_list.pop())
+print(my_list)
+my_list.sort()
+print(my.list)
+
+my_list.sort()
+print(my_list)
+
+my_list.reverse()
+print(my_list)
+
+my_list = [10,3,8,9,42,8,"hello"]
+list_tmp = [5,7,"world"]
+print(my_list, list_tmp)
+my_list.extend(list_tmp)
+print(my_list)
+clear(my_list)
+
+print(my_list.clear)
+print(my_list)
+list_tmp.clear()
+print(list_tmp)
+
+my_list.remove(3)
+print(my_list)
+
+my_list.remove(2)
+print(my_list)
+
+del my_list[2]
+print(my_list)
+
+
+my_tup = (4,2,6,1,"py","w")
+print(my_tup)
+print(my_tup[2]+my_tup[0])
+
+print(len(my_tup))
+print(my_tup.__len__())
+
+my_tup = (4,2,6,1,"py","w", ("h", 8, 7, 3))
+print(my_tup)
+
+print(my_tup[6][0])
+
+my_tup = (4,2,6,1,6,"py", "w")
+print(my_tup.index(2,0,3))
+
+print(my_tup.index(1,0,3))
+print(my_tup.index(1,0,6))
+
+print(my_tup.count[6])
+
+
+
+idx = ("key1", "key2", "key3")
+dicts = dict.fromkeys(idx, "0")
+print(dicts)
+dicts = dict.fromkeys(idx, "3")
+print(dicts)
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+print(my_dict["key1"])
+
+print(my_dict.get('key2'))
+my_str= my_dict.get('key2')
+print(my_str)
+
+print(my_dict.pop('key1'))
+print(my_dict)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+print(my_dict.pop("key1"))
+print(my_dict)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+dicts = my_dict.copy()
+print(dicts)
+print(my_dict)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+dicData = {"name": "python", "number": 23564897}
+
+my_dict['key3'] = "value3"
+print(my_dict)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+my_dict.setdefault("key3")
+print(my_dict)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+del my_dict["key2"]
+print(my_dict)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+my_dict.update({"key1": "v4"})
+print(my_dict)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+print("key2" in my_dict)
+print("key3" in my_dict)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+my_list = list(my_dict.keys())
+print(my_list)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+my_list = list(my_dict.values())
+print(my_list)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+my_set = set(my_dict.keys())
+print(my_set)
+
+my_dict = {"key1": "value1", "key2": "value2"}
+my_dict.clear()
+print(my_dict)
+
+
+
+
